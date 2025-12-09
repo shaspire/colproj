57c0646588a1f8d0de70d0ce248a17ba90b51d98
shaspire <233701807+shaspire@users.noreply.github.com>
2025-12-09 10:28:04 +0500

    Make number keys change workplace

--- main.js
+++ main.js
@@ -49,9 +49,15 @@ const time = document.getElementById("time");
 setColorscheme(Object.keys(COLORSCHEMES)[RandomInt(0,Object.keys(COLORSCHEMES).length-1)]);
 setBackground(Object.keys(BACKGROUNDS)[RandomInt(0,Object.keys(BACKGROUNDS).length-1)]);
 
-oncePerSecond(() => {time.textContent = new Date().toTimeString().slice(0,8);});
+oncePerSecond(() => {time.textContent = new Date().toTimeString().slice(0,8)});
 
 for (const name in COLORSCHEMES) {colorsheme_select.insertAdjacentHTML("beforeend",`<option value="${name}">${name}</option>`)}
 for (const name in BACKGROUNDS) {background_select.insertAdjacentHTML("beforeend",`<option value="${name}">${name}</option>`)}
 colorsheme_select.onchange = () => {setColorscheme(colorsheme_select.value)}
 background_select.onchange = () => {setBackground(background_select.value)}
+
+addEventListener("keydown", (event) => {
+	if ([1,2,3,4,5].includes(+(event.key)) && !event.repeat) {
+		switchDesktop(+(event.key)-1);
+	}
+})
